7ec2e572a65b4ae57aa8f4e307ff4f046a7ab5ae
Ahror Jabborov <ahror.jabborov@icloud.com>
2022-06-07 10:17:13 +0300

    Initial Commit

---
+++ ReusEd/ReusEdApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReusEdApp.swift
+//  ReusEd
+//
+//  Created by Ahror Jabborov on 6/7/22.
+//
+
+import SwiftUI
+
+@main
+struct ReusEdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
