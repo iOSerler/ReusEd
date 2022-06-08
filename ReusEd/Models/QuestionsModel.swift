8112a5ad03d010e80b1eaac7227083c8252ec246
AnnaDluzhinskaya <a.dluzhinskaya@gmail.com>
2022-06-08 14:26:11 +0300

    add question data structure

--- ReusEd/Models/QuestionsModel.swift
+++ ReusEd/Models/QuestionsModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct QuestionData{
+    var stepsImage: String
+    var title: String
+    var titleFont: String
+    var titleColor: String
+    var description: String
+    var descriptionFont: String
+    var descriptionColor: String
+    var optionsData: [[String]]
+    var optionsColor: String
+    var buttonColor: String
+    var buttonTextColor: String
+}
+
+var question1 = QuestionData(stepsImage: "Question1", title: "How did your hear about us?", titleFont: "", titleColor: "", description: "Tell us how you new about ReusEd to recommend it to more learners like you.", descriptionFont: "", descriptionColor: "", optionsData: [["", "", ], ["", ""]], optionsColor: "", buttonColor: "", buttonTextColor: "")
--- ReusEd/Views/Onboarding/PersonalizationQuestions/Question1View.swift
+++ ReusEd/Views/Onboarding/PersonalizationQuestions/Question1View.swift
@@ -9,26 +9,42 @@ import SwiftUI
 
 struct Question1View: View {
     var body: some View {
-        VStack(alignment: .center, spacing: UIScreen.main.bounds.width/15){
-            Image("Question1")
-            
-            Text("How did your hear about us?")
-                .font(Font.custom("Rubik-Medium", size: 20))
-                .foregroundColor(Color(notificationPermissionData.titleColor))
-                .multilineTextAlignment(.center)
-                .fixedSize(horizontal: false, vertical: true)
-                .frame(idealHeight: .infinity)
-            
-            Text("Tell us how you new about ReusEd to recommend it to more learners like you.")
-                .font(Font.custom("Rubik-Regular", size: 14))
-                .foregroundColor(Color(notificationPermissionData.descriptionColor))
-                .multilineTextAlignment(.center)
-                .fixedSize(horizontal: false, vertical: true)
-                .frame(idealHeight: .infinity)
-                .padding(.horizontal)
-            
-            Spacer()
-        }.padding(.top, UIScreen.main.bounds.height/20)
+        NavigationView{
+            VStack(alignment: .center, spacing: UIScreen.main.bounds.width/15){
+                Image(question1.stepsImage)
+                
+                Text(question1.title)
+                    .font(Font.custom("Rubik-Medium", size: 20))
+                    .foregroundColor(Color(notificationPermissionData.titleColor))
+                    .multilineTextAlignment(.center)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .frame(idealHeight: .infinity)
+                
+                Text(question1.description)
+                    .font(Font.custom("Rubik-Regular", size: 14))
+                    .foregroundColor(Color(notificationPermissionData.descriptionColor))
+                    .multilineTextAlignment(.center)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .frame(idealHeight: .infinity)
+                    .padding(.horizontal)
+                
+                List{
+                    
+                }
+                
+                Spacer()
+                
+                Text("Continue")
+                    .frame(width: UIScreen.main.bounds.width - 60, height: 50, alignment: .center)
+                    .background(Color(notificationPermissionData.buttonColor))
+                    .accentColor(Color(notificationPermissionData.buttonTextColor))
+                    .cornerRadius(UIScreen.main.bounds.width/35)
+                    .padding(.bottom, UIScreen.main.bounds.height/30)
+                
+                
+            }.padding(.top, UIScreen.main.bounds.height/20)
+             .navigationBarHidden(true).transition(.opacity)
+        }
     }
 }
 
