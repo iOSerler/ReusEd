816778fb4d42625039d18a14a764685142f50bac
AnnaDluzhinskaya <a.dluzhinskaya@gmail.com>
2022-06-09 11:00:50 +0300

    add tabBar views/model

--- ReusEd/Views/Home/HomeView.swift
+++ ReusEd/Views/Home/HomeView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct HomeView: View {
-    var body: some View {
-        Text("Here will be home tab view")
-    }
+    @State private var selection = 0
+        var body: some View {
+            TabView(selection: $selection) {
+                Text("Home")
+                    .tabItem {
+                        selection == 0 ? Image(systemName: "house.fill") : Image(systemName: "house")
+                        Text("Home")
+                    }
+                    .tag(0)
+
+                Text("Away")
+                    .tabItem {
+                        selection == 1 ? Image(systemName: "a.circle.fill") : Image(systemName: "hand.raised.fill")
+                        Text("Away")
+                    }
+                    .tag(1)
+                
+                Text("Away2")
+                    .tabItem {
+                        selection == 2 ? Image(systemName: "a.circle.fill") : Image(systemName: "hand.raised.fill")
+                        Text("Away")
+                    }
+                    .tag(2)
+                
+                Text("Away3")
+                    .tabItem {
+                        selection == 3 ? Image(systemName: "a.circle.fill") : Image(systemName: "hand.raised.fill")
+                        Text("Away")
+                    }
+                    .tag(3)
+            }
+        }
+
 }
 
 struct HomeView_Previews: PreviewProvider {
---
+++ ReusEd/Views/Home/Tab2TestView.swift
@@ -0,0 +1,20 @@
+//
+//  Tab2TestView.swift
+//  ReusEd
+//
+//  Created by Anna Dluzhinskaya on 09.06.2022.
+//
+
+import SwiftUI
+
+struct Tab2TestView: View {
+    var body: some View {
+        Text("Tab 2")
+    }
+}
+
+struct Tab2TestView_Previews: PreviewProvider {
+    static var previews: some View {
+        Tab2TestView()
+    }
+}
