d3da13f99d5fb8974f2598908192d1923f69bcc2
Ahror Jabborov <71430474+ahrorjabborov@users.noreply.github.com>
2022-06-15 21:12:11 +0300

    course Detailed added

---
+++ ReusEd/Views/Home/CourseContentViews/CourseDetailSectionCellView.swift
@@ -0,0 +1,38 @@
+//
+//  CourseDetailSectionCellView.swift
+//  ReusEd
+//
+//  Created by Ahror Jabborov on 6/15/22.
+//
+
+import SwiftUI
+
+struct CourseDetailSectionCellView: View {
+    @State var section: CourseSection
+    var body: some View {
+        
+        HStack {
+            VStack(alignment: .leading) {
+                Text(section.title)
+                    .font(.custom(courseDetailAssets.sectionNameFont!, size: 14))
+                    .foregroundColor(Color(courseDetailAssets.sectionNameColor!))
+                ForEach(section.topics) { topic in
+                    CourseDetailTopicCellView(topic: topic)
+                    Divider()
+                        .padding(.vertical, 20)
+                }
+                
+            }
+            
+            Spacer()
+        }
+        
+        
+    }
+}
+
+struct CourseDetailSectionCellView_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseDetailSectionCellView(section: courses[0].detail!.sections[0])
+    }
+}
