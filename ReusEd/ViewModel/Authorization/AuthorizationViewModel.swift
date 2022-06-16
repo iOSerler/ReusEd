a36a18b16e9222327f8b129100e64d2888e1547f
Danila Goriachkin <dndv279@gmail.com>
2022-06-16 14:20:16 +0300

    Add auto authorization type selection

--- ReusEd/ViewModel/Authorization/AuthorizationViewModel.swift
+++ ReusEd/ViewModel/Authorization/AuthorizationViewModel.swift
@@ -19,19 +19,41 @@ class AuthorizationViewModel: ObservableObject {
         case signIn, signUp
     }
 
-    @Published var currentType: AuthorizationForm = .signIn
+    @Published var currentType: AuthorizationForm =
+        AuthorizationViewModel.wasAuthorized ? .signIn : .signUp
 
     var onAuthorized: ((AuthorizationResult) -> Void)?
 
     // TODO:
     func signUp(name: String, email: String, password: String) {
-        let user = AuthorizationResult(token: "", type: .signUp)
-        onAuthorized?(user)
+        let result = AuthorizationResult(token: "", type: .signUp)
+        completeAuthorization(with: result)
     }
 
     // TODO:
     func signIn(email: String, password: String) {
-        let user = AuthorizationResult(token: "", type: .signIn)
-        onAuthorized?(user)
+        let result = AuthorizationResult(token: "", type: .signIn)
+        completeAuthorization(with: result)
+    }
+
+    private func completeAuthorization(with result: AuthorizationResult) {
+        AuthorizationViewModel.wasAuthorized = true
+        onAuthorized?(result)
+    }
+}
+
+extension AuthorizationViewModel {
+    private enum UserDefaultKeys {
+        static let wasAuthorized = "wasAuthorized"
+    }
+
+    /// True if user was authorized at least once
+    static private var wasAuthorized: Bool {
+        get {
+            UserDefaults.standard.bool(forKey: UserDefaultKeys.wasAuthorized)
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: UserDefaultKeys.wasAuthorized)
+        }
     }
 }
