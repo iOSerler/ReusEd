b6fd3b6513c31355dbafb606c2e83168f6de791a
Ahror Jabborov <71430474+ahrorjabborov@users.noreply.github.com>
2022-06-18 12:50:07 +0300

    TextFields added next buttons, skip button on introduction pages fixed

--- ReusEd/Views/Authorization/SignInView.swift
+++ ReusEd/Views/Authorization/SignInView.swift
@@ -12,15 +12,23 @@ struct SignInView: View {
     @State private var email: String = ""
     @State private var password: String = ""
     @State private var showingAlert = false
-
+    
     @State private var isSecured: Bool = true
+    @FocusState private var focusedField: SignInField?
+    
+    enum SignInField {
+        case email
+        case password
+    }
+    
+    
     var body: some View {
-
+        
         VStack {
-
+            
             VStack(alignment: .center, spacing: UIScreen.main.bounds.width/15) {
                 Image(notificationPermissionData.logoImage)
-
+                
                 Text("Welcome Back!")
                     .font(Font.custom(notificationPermissionData.titleFont, size: 20))
                     .foregroundColor(Color(notificationPermissionData.titleColor))
@@ -35,31 +43,36 @@ struct SignInView: View {
                     .frame(idealHeight: .infinity)
                     .padding(.horizontal)
             }
-
+            
             VStack {
                 HStack {
                     Image("envelope")
-
+                    
                     TextField("Email", text: $email)
                         .textFieldStyle(PlainTextFieldStyle())
+                        .focused($focusedField, equals: .email)
+                        .textContentType(.emailAddress)
                         .font(.custom("Rubik-Regular", size: 14))
                         .autocapitalization(.none)
                         .disableAutocorrection(true)
-
+                    
                         .padding(.leading, 12)
                         .frame(width: UIScreen.main.bounds.width - 100, alignment: .leading)
                         .keyboardType(.emailAddress)
+                        .submitLabel(.next)
                     Spacer()
-
+                    
                 }
-
+                
                 Divider()
-
+                
                 HStack {
                     Image("lock")
-
+                    
                     if self.isSecured {
                         SecureField("Password", text: $password)
+                            .focused($focusedField, equals: .password)
+                            .textContentType(.password)
                             .textFieldStyle(PlainTextFieldStyle())
                             .font(.custom("Rubik-Regular", size: 14))
                             .autocapitalization(.none)
@@ -67,8 +80,10 @@ struct SignInView: View {
                             .padding(.leading, 12)
                             .frame(width: UIScreen.main.bounds.width / 1.6, alignment: .leading)
                             .keyboardType(.default)
+                            .submitLabel(.done)
                     } else {
                         TextField("Password", text: $password)
+                            .focused($focusedField, equals: .password)
                             .textFieldStyle(PlainTextFieldStyle())
                             .font(.custom("Rubik-Regular", size: 14))
                             .autocapitalization(.none)
@@ -76,17 +91,18 @@ struct SignInView: View {
                             .padding(.leading, 12)
                             .frame(width: UIScreen.main.bounds.width / 1.6, alignment: .leading)
                             .keyboardType(.default)
+                            .submitLabel(.done)
                     }
-
+                    
                     Spacer()
                     Button(
                         action: {
-                                    isSecured.toggle()
-                                },
-                         label: {
-                                  Image(systemName: self.isSecured ? "eye.slash" : "eye")
-                                      .accentColor(Color("Grey"))
-                              }
+                            isSecured.toggle()
+                        },
+                        label: {
+                            Image(systemName: self.isSecured ? "eye.slash" : "eye")
+                                .accentColor(Color("Grey"))
+                        }
                     )
                 }
                 .padding(.top, 20)
@@ -94,18 +110,22 @@ struct SignInView: View {
             }
             .padding(.top, 50)
             .padding(.horizontal, 20)
-
-            NavigationLink(destination: HomeView()) {
-                HStack {
-                    Spacer()
+            HStack {
+                Spacer()
+                NavigationLink(destination: ForgotPasswordView()) {
+                    
                     Text("Forgot password?")
                         .font(Font.custom(notificationPermissionData.descriptionFont, size: 14))
-                    .foregroundColor(Color(notificationPermissionData.titleColor))
+                        .foregroundColor(Color(notificationPermissionData.titleColor))
+                    
                 }
+                .frame(width: UIScreen.main.bounds.width / 3)
+                .padding(.top, 16)
+                .padding(.trailing, 20)
             }
-            .padding(.top, 16)
-            .padding(.trailing, 20)
-
+            
+            
+            
             Spacer()
             Button(action: {
                 let password = UserDefaults.standard.value(forKey: "Password") as? String ?? ""
@@ -115,7 +135,7 @@ struct SignInView: View {
                 } else {
                     self.showingAlert = password != self.password || email != self.email || email.isEmpty ? true : false
                 }
-
+                
                 if  !self.showingAlert {
                     DispatchQueue.main.async {
                         withAnimation {
@@ -133,14 +153,14 @@ struct SignInView: View {
                     .padding(.bottom, UIScreen.main.bounds.height/60)
             })
             .alert("Wrong email or password", isPresented: $showingAlert) {
-                       Button("OK", role: .cancel) { }
-                   }
-
+                Button("OK", role: .cancel) { }
+            }
+            
             HStack(alignment: .center) {
                 Text("Don’t have an account yet? ")
                     .font(Font.custom(notificationPermissionData.descriptionFont, size: 14))
                     .foregroundColor(Color("MainText"))
-
+                
                 NavigationLink(destination: SignUpView()
                     .navigationBarBackButtonHidden(false).navigationBarHidden(false)) {
                         Text("Sign Up")
@@ -151,6 +171,14 @@ struct SignInView: View {
             .padding(.bottom, 10)
         }
         .navigationBarTitleDisplayMode(.inline)
+        .onSubmit {
+            switch focusedField {
+            case .email:
+                focusedField = .password
+            default:
+                print("Logging in…")
+            }
+        }
     }
 }
 
--- ReusEd/Views/Onboarding/IntroductionPagesView.swift
+++ ReusEd/Views/Onboarding/IntroductionPagesView.swift
@@ -21,13 +21,13 @@ struct IntroductionPagesView: View {
                                 .scaledToFit()
                                 .frame(height: UIScreen.main.bounds.height / 3)
                             //                                    .padding(.top, UIScreen.main.bounds.height / 15)
-
+                            
                             Text(page.title)
                                 .font(.custom(page.titleFont, size: 18))
                                 .foregroundColor(Color(page.titleColor))
                                 .multilineTextAlignment(.center)
                                 .padding(.top, 30)
-
+                            
                             Text(page.description)
                                 .font(.custom(page.descriptionFont, size: 14))
                                 .foregroundColor(Color(page.descriptionColor))
@@ -41,9 +41,9 @@ struct IntroductionPagesView: View {
             }
             .edgesIgnoringSafeArea(.top)
             .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-
+            
             HStack(spacing: 12) {
-
+                
                 ForEach(0..<introductionPagesData.count, id: \.self) { index in
                     Capsule()
                         .foregroundColor(
@@ -54,7 +54,7 @@ struct IntroductionPagesView: View {
                         .frame(width: selectedIndex == index ? 16 : 8, height: 8)
                 }
             }
-
+            
             Button(action: {
                 if selectedIndex < introductionPagesData.count - 1 {
                     withAnimation {
@@ -72,34 +72,35 @@ struct IntroductionPagesView: View {
                     Text(selectedIndex != introductionPagesData.count - 1 ? "Next" : "Get Started")
                         .font(.custom(introductionPagesData[0].titleFont, size: 16))
                         .foregroundColor(Color(introductionPagesData[0].buttonTextColor))
-
+                    
                     Image(introductionPagesData[0].buttonImage)
                         .padding(.leading, 20)
                 }
-
-                    .font(Font.custom("Rubik-Medium", size: 16))
-                    .frame(width: UIScreen.main.bounds.width - 60, height: 50, alignment: .center)
-                    .background(Color(introductionPagesData[0].buttonColor))
-                    .accentColor(Color(introductionPagesData[0].buttonTextColor))
-                    .cornerRadius(UIScreen.main.bounds.width/35)
+                
+                .font(Font.custom("Rubik-Medium", size: 16))
+                .frame(width: UIScreen.main.bounds.width - 60, height: 50, alignment: .center)
+                .background(Color(introductionPagesData[0].buttonColor))
+                .accentColor(Color(introductionPagesData[0].buttonTextColor))
+                .cornerRadius(UIScreen.main.bounds.width/35)
             })
             .padding(.top, 15)
             .padding(.bottom, UIScreen.main.bounds.height/30)
         }
         .navigationBarItems(trailing:
-                                NavigationLink(
-                                    destination:
-                                        DailyNotificationsPermissionView()
-                                        .navigationBarBackButtonHidden(true)
-                                        .navigationBarHidden(true),
-                                    label: {
-                                        Text("skip")
-                                            .foregroundColor(
-                                                selectedIndex != introductionPagesData.count - 1 ?
-                                                    Color(introductionPagesData[0].descriptionColor) : Color.clear
-                                            )
-                                            .font(.custom("Rubik-Regular", size: 14))
-                                    }).disabled(selectedIndex == introductionPagesData.count - 1)
+                                
+                                Button(action: {
+            viewRouter.showNotificationPermission()
+            
+        }, label: {
+            Text("skip")
+                .foregroundColor(
+                    selectedIndex != introductionPagesData.count - 1 ?
+                    Color(introductionPagesData[0].descriptionColor) : Color.clear
+                )
+                .font(.custom("Rubik-Regular", size: 14))
+        }).disabled(selectedIndex == introductionPagesData.count - 1)
+                            
+                            
         )
         .navigationBarBackButtonHidden(true)
     }
