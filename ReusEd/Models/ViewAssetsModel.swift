dab35ddca697aa4bed0f6cc366726b3460252175
AnnaDluzhinskaya <a.dluzhinskaya@gmail.com>
2022-06-20 11:17:32 +0300

    add view assets model

---
+++ ReusEd/Models/ViewAssetsModel.swift
@@ -0,0 +1,38 @@
+//
+//  ViewAssetsModel.swift
+//  ReusEd
+//
+//  Created by Anna Dluzhinskaya on 20.06.2022.
+//
+
+import Foundation
+
+
+struct ViewAssets{
+    var titleFont: String
+    var descriptionFont: String
+    var mainTextColor: String
+    var descriptionTextColor: String
+    var detailsTextColor: String
+    var copyrightTextColor: String
+    var placeholderTextColor: String
+    var primaryColor: String
+    var primaryLightColor: String
+    var primaryLighterColor: String
+    var buttonTextColor: String
+    var borderColor: String
+}
+
+var viewAssets = ViewAssets(
+    titleFont: "Rubik-Medium",
+    descriptionFont: "Rubik-Regular",
+    mainTextColor: "#36365C",
+    descriptionTextColor: "#5F6575",
+    detailsTextColor: "#9B9EA9",
+    copyrightTextColor: "#BABEC5",
+    placeholderTextColor: "#7D828F",
+    primaryColor: "#6146C6",
+    primaryLightColor: "#816BD1",
+    primaryLighterColor: "#DFDAF4",
+    buttonTextColor: "#FFFFFF",
+    borderColor: "#F1F1F3")
