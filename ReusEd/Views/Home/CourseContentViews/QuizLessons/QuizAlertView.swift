954e426a46514831c3048cdada9eabe93468a1f5
AnnaDluzhinskaya <a.dluzhinskaya@gmail.com>
2022-06-29 13:50:36 +0300

    add alertView

--- ReusEd/Views/Home/CourseContentViews/QuizLessons/QuizAlertView.swift
+++ ReusEd/Views/Home/CourseContentViews/QuizLessons/QuizAlertView.swift
@@ -8,13 +8,53 @@
 import SwiftUI
 
 struct QuizAlertView: View {
+    var settings: ViewAssets
+    @Binding var showAlert: Bool
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image(settings.quizPopup)
+                .scaledToFit()
+            VStack(alignment: .center, spacing: 18) {
+                Image(settings.quizPopupImage)
+                Text("Opps, Wait a sec!")
+                    .font(.custom(settings.titleFont, size: 16))
+                    .foregroundColor(Color(settings.mainTextColor))
+                Group {
+                    Text("You can not pass the quiz before you complete at least ")
+                        .font(.custom(settings.descriptionFont, size: 14))
+                        .foregroundColor(Color(settings.descriptionTextColor))
+                    +
+                    Text("80% of the course. ")
+                        .font(.custom(settings.descriptionFont, size: 14))
+                        .foregroundColor(Color(settings.primaryColor))
+                    +
+                    Text("Go back and learn a little more and come take quiz.")
+                        .font(.custom(settings.descriptionFont, size: 14))
+                        .foregroundColor(Color(settings.descriptionTextColor))
+                }.multilineTextAlignment(.center)
+                 .fixedSize(horizontal: false, vertical: true)
+                
+                Button(
+                    action: {
+                        showAlert.toggle()
+                    }, label: {
+                        Text("Go Back to Course")
+                            .font(Font.custom(settings.titleFont, size: 14))
+                            .frame(width: UIScreen.main.bounds.height/2.5 - 60, height: 45, alignment: .center)
+                            .background(Color(settings.primaryColor))
+                            .accentColor(Color(settings.buttonTextColor))
+                            .cornerRadius(UIScreen.main.bounds.width/35)
+                            .padding(.bottom, UIScreen.main.bounds.height/30)
+                    }
+                )
+                
+            }.padding(.horizontal, UIScreen.main.bounds.width/10)
+        }.frame(width: UIScreen.main.bounds.height/2.5, height: UIScreen.main.bounds.height/2.5, alignment: .center)
     }
 }
 
-struct QuizAlertView_Previews: PreviewProvider {
-    static var previews: some View {
-        QuizAlertView()
-    }
-}
+//struct QuizAlertView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        QuizAlertView(settings: viewAssets)
+//    }
+//}
